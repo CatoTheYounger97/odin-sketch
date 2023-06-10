a141461afefb48eaade149faa3d1c54bbd5b8af8
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-10 13:02:46 +0100

    Test create DOM elemetns

---
+++ index.js
@@ -0,0 +1,4 @@
+const gridDiv = document.createElement("div");
+const gridRowOne = document.createElement("div");
+const gridRowTwo = document.createElement("div");
+const gridRowThree = document.createElement("div");
